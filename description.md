4ea7ae91a584ed3f62bb20769bd4ab16d3fb14fc
bufferies98 <pavelcska1948@abv.bg>
2016-03-09 15:53:09 +0200

    asd

---
+++ description.md
@@ -0,0 +1,12 @@
+Milestone 1:
+1.Choose a project - Subtitle generator
+2.Choose a suitable programming language- Python 
+3.Choose an environment - Text Editor + Terminal
+4.Setup the chosen environment 
+5.Choose the appropriate API for speech recognition - Google Speech Recognition Api
+6.Search for libraries for that API for the used language - speech_recognition
+7.Setup the library  
+8.Make it work
+9.Test with some videos
+10.Debuging
+11.Check if it returns the correct letters
--- wav_transcribe.py
+++ wav_transcribe.py
@@ -1,15 +1,16 @@
 import speech_recognition as sr
 from datetime import datetime
 startTime = datetime.now()
+#for checking the execution time
 from os import path
-WAV_FILE = path.join(path.dirname(path.realpath(__file__)), "output_audio.wav")
-r = sr.Recognizer()
+WAV_FILE = path.join(path.dirname(path.realpath(__file__)), "test.wav")
+r = sr.Recognizer()#initializing a recognizer
 with sr.WavFile(WAV_FILE) as source:
-    audio = r.record(source)
+    audio = r.record(source)#get the waveform from the wav file
 try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-    print datetime.now() - startTime
+    print(r.recognize_google(audio))#prints the recognized words
+    print datetime.now() - startTime#prints the execution time
 except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
+    print()#throwing an error if the speech is not understandable 
 except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    print(format(e))#throwing error if there is problem with the connection to the api
