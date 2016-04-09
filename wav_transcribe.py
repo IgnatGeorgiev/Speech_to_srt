a57dfd5cbd6c22587891a8ef11b824ca3490e8e3
Ignat Georgiev <ignatgeorgiev1@gmail.com>
2016-04-09 19:07:10 +0300

    Milestone 2: readygit add wav_transcribe.py  Now working for whole audio and video filesgit add wav_transcribe.py

--- wav_transcribe.py
+++ wav_transcribe.py
@@ -1,16 +1,35 @@
 import speech_recognition as sr
-from datetime import datetime
-startTime = datetime.now()
-#for checking the execution time
+import wave
+import os
+import contextlib
+import sys
 from os import path
-WAV_FILE = path.join(path.dirname(path.realpath(__file__)), "test.wav")
-r = sr.Recognizer()#initializing a recognizer
-with sr.WavFile(WAV_FILE) as source:
-    audio = r.record(source)#get the waveform from the wav file
-try:
-    print(r.recognize_google(audio))#prints the recognized words
-    print datetime.now() - startTime#prints the execution time
-except sr.UnknownValueError:
-    print()#throwing an error if the speech is not understandable 
-except sr.RequestError as e:
-    print(format(e))#throwing error if there is problem with the connection to the api
+WAV_FILE = path.join(path.dirname(path.realpath(__file__)), sys.argv[1])
+extension = ""
+extension = WAV_FILE.split(".")[-1]
+text_file =("sample.txt","w")
+if (extension != "wav"):
+    os.system("avconv -i "+WAV_FILE+" -vn -f wav temp.wav")
+    WAV_FILE  = "temp.wav"
+fname = WAV_FILE
+with contextlib.closing(wave.open(fname,'r')) as f:
+    frames = f.getnframes()
+    rate = f.getframerate()
+    duration = frames / float(rate)
+    end = duration
+r = sr.Recognizer()
+start = 0.0;
+print(end)
+while(start != end):
+    with sr.WavFile(WAV_FILE) as source:
+        audio = r.record(source,4.0, start)
+        start += 4.0
+    try:
+        text_file.write(recognize_google(audio))
+        print(r.recognize_google(audio)),
+
+    except sr.UnknownValueError:
+        print "",
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+os.remove("temp.wav")
