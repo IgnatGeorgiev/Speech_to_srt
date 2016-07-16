8a251954f9105ad38b28f452f62d57df9a129926
IgnatGeorgiev <ignatgeorgiev1@gmail.com>
2016-07-17 00:54:03 +0300

    Adding a new process to the program, fixing a bug where the UI froze

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
-
+from multiprocessing import Process
+import speech_recognition as sr
+import wave
+import os
+import contextlib
+import sys
+from os import path
 import os
 import speech_recognition as sr
 import wave
@@ -11,6 +17,7 @@ import os
 import contextlib
 import sys
 from os import path
+
 path_dir = ""
 class ConvertDialog(FloatLayout):
  	convert = ObjectProperty(None)
@@ -28,9 +35,12 @@ class Root(FloatLayout):
      	self._popup.open();
 
     def convert(self,path,filename):
-    	path_dir = os.path.join(path, filename[0])
-        os.system("python wav_transcribe.py " + path_dir+" > new_file.srt")
-    	self.dismiss_popup()
+		path_dir = os.path.join(path, filename[0])
+		pr = Process(target=function, args=(path_dir, "new_file.srt"))
+		print path_dir
+		pr.start()
+		pr.join()
+		self.dismiss_popup()
         
 
 class Editor(App):
@@ -38,6 +48,40 @@ class Editor(App):
 
 Factory.register('Root', cls=Root)
 Factory.register('ConvertDialog', cls=ConvertDialog)
+
+def function(path_dir, ext_file):
+    i = 0
+    
+    WAV_FILE = path.join(ext_file)
+    extension = ""
+    extension = WAV_FILE.split(".")[-1]
+    if (extension != "wav"):
+        os.system("avconv -i " + WAV_FILE + "  -vn -f wav temp.wav")
+        WAV_FILE  = "temp.wav"
+    fname = WAV_FILE
+    with contextlib.closing(wave.open(fname,'r')) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+        duration = frames / float(rate)
+        end = duration
+    r = sr.Recognizer()
+    start = 0.0;
+    end_time = 4.0;
+    while(start != end):
+        i+=1
+        with sr.WavFile(WAV_FILE) as source:
+            audio = r.record(source,end_time, start)
+            start += 4.0
+            end_time += 4.0
+        try:
+            print '\n', i,'\n',start," --> ",end_time , r.recognize_google(audio),
+        except sr.UnknownValueError:
+            print "",
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        os.remove("temp.wav")
+    return 0
+
 
 if __name__ == '__main__':
     Editor().run()
